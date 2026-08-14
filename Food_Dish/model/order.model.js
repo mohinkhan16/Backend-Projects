3e7da781122858222d8052e71463162c53bfe096
Mohin Pathan <mohinpathan2004@gmail.com>
2026-08-14 11:12:44 +0530

    order model added

---
+++ Food_Dish/model/order.model.js
@@ -0,0 +1,46 @@
+
+import mongoose from "mongoose"
+
+const OrderSchema = new mongoose.Schema({
+
+    customerName:{
+      type:String,
+      required:true
+    },
+    Address:{
+        type:String,
+        required:true
+    },
+    resturantName:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Restaurant",
+    },
+    food:[{
+        foodId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"food",
+            required:true,
+        },
+        qty:{
+            type:Number,
+            requried:true,
+            min:1,
+        },
+    }],
+    orderStatus:{
+        type:String,
+        enum:[
+            "Pending",
+            "Confirmed",
+            "Otw",
+            "Cancelled",
+            "preparing"
+        ],
+        default:"Pending"
+    }
+},{
+    timestemps:true
+})
+
+const orderModel = mongoose.model("order",OrderSchema);
+export default orderModel;
